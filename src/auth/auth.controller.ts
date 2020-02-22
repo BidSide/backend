6be31c6c76aba1aaa3d5d0cb0d5ffd4270e03e46
drendragon <n.sandor93@gmail.com>
2020-02-22 17:09:46 +0100

    enhanvements

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, UseGuards, Request, Get, Body } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
+import { UserDto } from '../users/dto/user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,16 +13,19 @@ export class AuthController {
   ) {
   }
 
-  @Post('/signup')
+  @Post('signup')
   async createUserByEmailAndPassword(
     @Request() req,
-    @Body() createUserDto: any,
+    @Body() createUserDto: UserDto,
   ) {
     return await this.usersService.save(req, createUserDto);
   }
 
   @Post('login')
-  async login(@Request() req, @Body() user: any,) {
+  async login(
+    @Request() req,
+    @Body() user: UserDto,
+  ) {
     return this.authService.login(user);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(helmet());
+  app.setGlobalPrefix('api/v1.0');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors({
+    exposedHeaders: [
+      'X-Total-Count',
+    ],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
