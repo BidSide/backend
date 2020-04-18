695eef9e7a082264e754ce415afb34a18b23447f
drendragon <n.sandor93@gmail.com>
2020-04-18 14:12:36 +0200

    product CRUD

---
+++ src/product/product.service.ts
@@ -0,0 +1,53 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ProductDto } from './dto/product.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ProductInterface } from './interfaces/product.interface';
+
+@Injectable()
+export class ProductService {
+
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<ProductInterface>,
+  ) {
+  }
+
+  async getAllProduct() {
+    return this.productModel.find();
+  }
+
+  async createProduct(req, productDto: ProductDto) {
+    const product = new this.productModel(productDto);
+    product.profile = req.user;
+
+    return await product.save();
+  }
+
+  async getProductById(_id: string) {
+    return this.productModel.findById(_id);
+  }
+
+  async updateProduct(req, _id: string, productDto: ProductDto) {
+    const productInDatabase = await this.getProductById(_id);
+
+    if (productInDatabase.profile._id !== req.user._id) {
+      throw new BadRequestException();
+    }
+
+    Object.assign(productInDatabase, productDto);
+
+    return await productInDatabase.save();
+
+  }
+
+  async deleteProduct(req, _id: string) {
+
+    const productInDatabase = await this.getProductById(_id);
+
+    if (productInDatabase.profile._id !== req.user._id) {
+      throw new BadRequestException();
+    }
+
+    return productInDatabase.remove();
+  }
+}
--- www/categories/interfaces/category.interface.js
+++ www/categories/interfaces/category.interface.js
@@ -1,3 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=category.interface.js.map
+//# sourceMappingURL=category.interfaces.js.map
