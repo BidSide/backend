153e45593d045ce55f2bb16837a940aee3e164e1
drendragon <n.sandor93@gmail.com>
2020-04-29 17:06:42 +0200

    CD

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,10 @@ FROM node:latest
 
 WORKDIR /app
 
-COPY package.json /app
-
 RUN npm install -g pm2
+
+COPY . .
+
 RUN npm install
 
-COPY dist /app/dist
-COPY ecosystem.config.js /app
-COPY www /app/www
-
-EXPOSE 3000
-CMD ["pm2-runtime", "ecosystem.config.js"]
+RUN npm run build
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,30 +11,6 @@ module.exports = {
       watch: false,
       // eslint-disable-next-line @typescript-eslint/camelcase
       max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-        jwtSecret: 'supersecret',
-        mongodbUser: 'rendszerf',
-        mongodbPswd: 'cBiuMYANMuMgv8H9',
-      },
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      env_production: {
-        NODE_ENV: 'production',
-        jwtSecret: 'supersecret',
-        mongodbUser: 'rendszerf',
-        mongodbPswd: 'cBiuMYANMuMgv8H9',
-      },
     },
   ],
-
-  // deploy : {
-  //   production : {
-  //     user : 'node',
-  //     host : '212.83.163.1',
-  //     ref  : 'origin/master',
-  //     repo : 'git@github.com:repo.git',
-  //     path : '/var/www/production',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-  //   }
-  // }
 };
